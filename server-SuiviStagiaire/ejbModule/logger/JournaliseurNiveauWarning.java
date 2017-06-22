6091db921c3c7a587c2f4f8ed94cfaefde8eed1a
Aurelien Harle <aurelienharle@hotmail.com>
2017-06-22 15:02:30 +0200

    [Create][LOGGER] ajout du niveau warning.

---
+++ server-SuiviStagiaire/ejbModule/logger/JournaliseurNiveauWarning.java
@@ -0,0 +1,66 @@
+package logger;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class JournaliseurNiveauWarning extends Logger {
+
+	private FileHandler handler;
+	private SimpleFormatter formatter;
+	private static final JournaliseurNiveauWarning INSTANCE = new JournaliseurNiveauWarning();
+	
+	/**
+	 * Never use it for Singleton pattern
+	 * 
+	 * @param name
+	 * @param resourceBundleName
+	 * @see JournaliseurNiveauWarning
+	 */
+	@Deprecated
+	protected JournaliseurNiveauWarning(String name, String resourceBundleName) {
+		super(name, resourceBundleName);
+	}
+
+
+	private JournaliseurNiveauWarning() {
+		super("LogWarn", null);
+		init();
+		
+	}
+
+	private void init() {
+
+		formatter = new SimpleFormatter(); 
+		try {
+			handler = new FileHandler("D:\\Projet\\Suivi stagiaire\\Git\\SuiviStagiaire\\server-SuiviStagiaire\\logs\\"+ LocalDate.now() + "_WARN_" + ".log");
+			handler.setFormatter(formatter); 
+			handler.setLevel(Level.WARNING);
+			this.setLevel(Level.WARNING);
+			addHandler(handler);
+		} catch (SecurityException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static JournaliseurNiveauWarning getINSTANCE() {
+		return INSTANCE;
+	}
+
+
+	public void log(String string) {
+		
+		String heure = LocalTime.now() + " ";
+		string = heure + string;
+		
+		this.log(Level.WARNING, string);
+		
+	}
+	
+}
