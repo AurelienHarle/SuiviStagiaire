fa89f96ee949b4ed2c630a8397515c274caad20b
Aurelien Harle <aurelienharle@hotmail.com>
2017-06-28 14:50:14 +0200

    [Create] Ajout de la classe ApplicationSupport qui est un ActionSupport

---
+++ client-SuiviStagiaire/src/autoEvaluation/ApplicationSupport.java
@@ -0,0 +1,9 @@
+package autoEvaluation;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class ApplicationSupport extends ActionSupport {
+
+	private static final long serialVersionUID = -7627973134167822846L;
+
+}
