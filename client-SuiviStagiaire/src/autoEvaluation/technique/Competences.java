30bbe95d1c6f9ae456ad3e116d18d0331b79bac2
Aurelien Harle <aurelienharle@hotmail.com>
2017-06-29 12:09:48 +0200

    [Create] Ajout des classe technique pour liste

---
+++ client-SuiviStagiaire/src/autoEvaluation/technique/Competences.java
@@ -0,0 +1,15 @@
+package autoEvaluation.technique;
+
+import java.util.ArrayList;
+
+import autoEvaluation.entity.Competence;
+
+public class Competences extends ArrayList<Competence>{
+
+	private static final long serialVersionUID = 7417437316824343701L;
+
+	public Competences() {
+
+	}
+
+}
