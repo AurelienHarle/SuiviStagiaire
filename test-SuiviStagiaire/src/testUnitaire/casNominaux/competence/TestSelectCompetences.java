51cd8b499c07f53228319c3eb5efcbfd0ff63144
Aurelien Harle <aurelienharle@hotmail.com>
2017-07-03 08:46:28 +0200

    [Create] Ajout d'un test pour inséré une competence

--- test-SuiviStagiaire/src/testUnitaire/casNominaux/competence/TestSelectCompetences.java
+++ test-SuiviStagiaire/src/testUnitaire/casNominaux/competence/TestSelectCompetences.java
@@ -8,7 +8,11 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import autoEvaluation.entity.Competence;
+import autoEvaluation.entity.Module;
+import autoEvaluation.entity.Sequence;
 import autoEvaluation.technique.Competences;
+import exception.DoublonException;
+import exception.NullException;
 import facade.FacadeSuiviStagiaireRemote;
 
 public class TestSelectCompetences {
@@ -17,6 +21,9 @@ public class TestSelectCompetences {
 	private static FacadeSuiviStagiaireRemote facadeSuiviStagiaireRemote;
 	private static Competences competences;
 	private boolean condition = true;
+	private Module module;
+	private Sequence sequence;
+	private Competence competence;
 	
 	@BeforeClass
 	public static void init() throws NamingException{
@@ -41,6 +48,36 @@ public class TestSelectCompetences {
 		
 		assertNotNull("Not null",competences);
 		assertTrue("Not empty",condition);
+	}
+	
+	@Test
+	public void testInsertCompetence(){
+		
+		module = new Module("M1","NomLong","NomCourt");
+		sequence = new Sequence("S1",module,"NomLong","NomCour");
+		competence = new Competence("C1", sequence,"NomLong","NomCour");
+		
+		try {
+			facadeSuiviStagiaireRemote.insertModule(module);
+		} catch (DoublonException | NullException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		try {
+			facadeSuiviStagiaireRemote.insertSequence(sequence);
+		} catch (DoublonException | NullException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		try {
+			facadeSuiviStagiaireRemote.insertCompetence(competence);
+		} catch (DoublonException | NullException e) {
+			
+			e.printStackTrace();
+			
+		}
 	}
 	
 	
