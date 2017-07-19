67413e4aadc6ffd6c76e46a797f64b71659fc36d
Aurelien Harle <aurelienharle@hotmail.com>
2017-07-19 11:24:49 +0200

    [Update] Réorganisation de package.

--- server-SuiviStagiaire/ejbModule/exception/dao/InsertNotUpdateException.java
+++ server-SuiviStagiaire/ejbModule/exception/dao/InsertNotUpdateException.java
@@ -1,4 +1,4 @@
-package exception;
+package exception.dao;
 
 import autoEvaluation.entity.AutoEvaluation;
 import logger.JournaliseurNiveauWarning;
--- server-SuiviStagiaire/ejbModule/exception/dao/NullException.java
+++ server-SuiviStagiaire/ejbModule/exception/dao/NullException.java
@@ -1,4 +1,4 @@
-package exception;
+package exception.dao;
 
 import logger.JournaliseurNiveauWarning;
 
