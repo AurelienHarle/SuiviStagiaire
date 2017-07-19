dcc498a9225ffc895939a58b2e32ec42b26d7ab0
Aurelien Harle <aurelienharle@hotmail.com>
2017-07-19 10:01:21 +0200

    [Create] SpecificationAction for action who need it.

---
+++ server-SuiviStagiaire/ejbModule/entity/requete/SpecificationAction.java
@@ -0,0 +1,23 @@
+package entity.requete;
+
+/**
+ * Enumeration des specification pour l'action select.
+ * TODO Jolie doc avec les possibilité de selection (service > action > spe).
+ * 
+ * @author Aurélien Harlé
+ * @version 1
+ * @since 19/07/2017
+ *
+ */
+public enum SpecificationAction {
+
+	BY_ID,
+	ALL,
+	BY_STAG,
+	BY_STAG_COMP_DATE,
+	BY_STAG_DATE,
+	BY_MULTIPLE_CRITERE,
+	BY_MODULE,
+	BY_SEQUENCE,
+	
+}
