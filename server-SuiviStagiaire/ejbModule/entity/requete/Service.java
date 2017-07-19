be906c2849844f4b9ad9a0835e7f4428b112865e
Aurelien Harle <aurelienharle@hotmail.com>
2017-07-19 10:00:15 +0200

    [Update] Rename.

---
+++ server-SuiviStagiaire/ejbModule/entity/requete/Service.java
@@ -0,0 +1,15 @@
+package entity.requete;
+
+/**
+ * Contient tout les noms des services que peux rendre l'application
+ * 
+ * @author Aurélien Harlé
+ * @version 1
+ * @since 19/07/2017
+ *
+ */
+public enum Service {
+	
+	PERSISTANCE
+
+}
