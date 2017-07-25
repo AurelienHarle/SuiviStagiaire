44d6846441e31e89a99cb435345737a7e5d19a4e
Harlé <aurelienharle@hotmail.com>
2017-07-25 15:29:05 +0200

    [Update] Ajout controle projet existant.

--- doc/Sauvegarde Projet.bat
+++ doc/Sauvegarde Projet.bat
@@ -1,35 +1,100 @@
 @echo off
-setlocal enableDelayedExpansion 
 REM @Author Aurélien Harlé
 REM @Version 1
 REM @Since 06-06-2017
-REM @Description Programme qui sauvegarde le projet en cour tout les jours a XX heure.
+REM @Description Programme qui sauvegarde le projet en cour dans un dossier dater a la date du jour et creer un fichier Zip dater lui aussi.
 
 REM Couleur officiel des batch d'Aurélien Harlé
-color 02
+	color 02
 
-REM Sauvegarde du projet
+REM Vérification de la presence du projet a sauvegardé
+	if not exist "D:\Projet\Suivi Stagiaire" goto EOF
+	
+REM Creation d'une copie pour sauvegarde qui permet l'utilisation de la shadow copie integré a robocopy
+	robocopy "D:\Projet\Suivi Stagiaire" "E:\Save\Projet\%date:~0,2%-%date:~3,2%-%date:~6,4%" /MIR
 
-REM Creation d'une copie pour sauvegarde
-robocopy "D:\Projet\Suivi Stagiaire" "E:\Save\Projet\%date:~0,2%-%date:~3,2%-%date:~6,4%" /E 
+REM Creation ou update dans un fichier zip daté
+	if exist E:\Save\Projet\%date:~0,2%-%date:~3,2%-%date:~6,4%\%date:~0,2%-%date:~3,2%-%date:~6,4%.zip (
 
-REM récuperation de l'heure et des minutes
-set /a heure = %time:~0,2%
-set /a minute = %time:~3,2%
+		7z u -tzip "E:\Save\Projet\%date:~0,2%-%date:~3,2%-%date:~6,4%\%date:~0,2%-%date:~3,2%-%date:~6,4%.zip" "E:\Save\Projet\%date:~0,2%-%date:~3,2%-%date:~6,4%"
+		
+	) else (
 
-REM Compression en zip de 25 mo
-REM 7z u -tzip -v25m "E:\Save\Projet\%date:~0,2%-%date:~3,2%-%date:~6,4%\save\%heure%-%minute%\%date:~0,2%-%date:~3,2%-%date:~6,4%.zip" "E:\Save\Projet\%date:~0,2%-%date:~3,2%-%date:~6,4%"
+		7z a -tzip "E:\Save\Projet\%date:~0,2%-%date:~3,2%-%date:~6,4%\%date:~0,2%-%date:~3,2%-%date:~6,4%.zip" "E:\Save\Projet\%date:~0,2%-%date:~3,2%-%date:~6,4%"
+		
+	)
+	
+REM Verifie la presence du support amovible
+	if not exist H:\nul goto EOF
 
-REM Compression en fichier zip
-7z a -tzip "E:\Save\Projet\%date:~0,2%-%date:~3,2%-%date:~6,4%\%date:~0,2%-%date:~3,2%-%date:~6,4%.zip" "E:\Save\Projet\%date:~0,2%-%date:~3,2%-%date:~6,4%"
+REM Creation ou update du fichier zip daté
+	:USB
+	if exist H:\Projet.zip (
+	
+		7z u -tzip "H:\Projet.zip" "E:\Save\Projet\%date:~0,2%-%date:~3,2%-%date:~6,4%"
+		
+	) else ( 
+	
+		7z a -tzip "H:\Projet.zip" "E:\Save\Projet\%date:~0,2%-%date:~3,2%-%date:~6,4%"
+	
+	)
+	
+REM Fin de script
+	:EOF
+	echo 
+	echo 
+	echo 
+	echo.
+	echo =======================
+	echo # Fin de la sauvegarde #
+	echo =======================
+	echo.
+	pause
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+REM setlocal enableDelayedExpansion 
 REM Supprime les fichier vieux de plus d'une semaine (5 jour ouvrés)
 REM forfiles /p "E:\Save\Projet\" /S /D -5
-
 REM rmdir /s /q "D:\Projet\Suivi stagiaire\Git\SuiviStagiaire\save\"
 REM copy  /Y "E:\Save\Projet\%date:~0,2%-%date:~3,2%-%date:~6,4%\save\%heure%-%minute%\%date:~0,2%-%date:~3,2%-%date:~6,4%.zip.*" "D:\Projet\Suivi stagiaire\Git\SuiviStagiaire\save\"
 REM git commit git push
+REM Compression en zip de 25 mo
+REM 7z u -tzip -v25m "E:\Save\Projet\%date:~0,2%-%date:~3,2%-%date:~6,4%\save\%heure%-%minute%\%date:~0,2%-%date:~3,2%-%date:~6,4%.zip" "E:\Save\Projet\%date:~0,2%-%date:~3,2%-%date:~6,4%"
 
-
-
-pause
